36a8543f767d543710bd53006af139cff23f854f
akodji <138826429+akodji@users.noreply.github.com>
2024-08-03 03:32:32 +0000

    Update event.py

--- event.py
+++ event.py
@@ -1,16 +1,50 @@
-from datetime import datetime
+from datetime import date, time
 
 class Event:
-    def __init__(self, id, title, date, time, location, description, priority):
-        self.id = id
+    def __init__(self, event_id, title, event_date, event_time, location, description, priority):
+        self.id = event_id
         self.title = title
-        self.date = date
-        self.time = time
+        self.date = event_date
+        self.time = event_time
         self.location = location
         self.description = description
         self.priority = priority
 
-    def __str__(self):
-        return (f"ID: {self.id}, Title: {self.title}, Date: {self.date}, Time: {self.time}, "
-                f"Location: {self.location}, Description: {self.description}, Priority: {self.priority}")
+    def set_title(self, title):
+        self.title = title
 
+    def set_date(self, event_date):
+        self.date = event_date
+
+    def set_time(self, event_time):
+        self.time = event_time
+
+    def set_location(self, location):
+        self.location = location
+
+    def set_description(self, description):
+        self.description = description
+
+    def set_priority(self, priority):
+        self.priority = priority
+
+    def get_id(self):
+        return self.id
+
+    def get_title(self):
+        return self.title
+
+    def get_date(self):
+        return self.date
+
+    def get_time(self):
+        return self.time
+
+    def get_location(self):
+        return self.location
+
+    def get_description(self):
+        return self.description
+
+    def get_priority(self):
+        return self.priority
